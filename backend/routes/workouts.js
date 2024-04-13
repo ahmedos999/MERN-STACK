e8226a54bfb7ccc6574e407cd943e591cea7955a
ahmed osman <ahmedos099912@gmail.com>
2024-04-13 15:50:08 +0400

    create a collection and post to it

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout =  require('../models/workoutModels')
 
 const router = express.Router()
 
@@ -14,8 +15,15 @@ router.get('/:id',(req,res)=>{
 })
 
 // Post new workout
-router.post('/',(req,res)=>{
-    res.json({mssg:'Post single workouts'})
+router.post('/',async (req,res)=>{
+    const {title, load,reps} = req.body
+
+    try{
+        const workout = await Workout.create({title,load,reps})
+        res.status(200).json(workout)
+    }catch(error){
+        res.status(400).json({error:error.message})
+    }
 })
 
 // Delete a workout
