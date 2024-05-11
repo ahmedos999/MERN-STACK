9eedebb9fba8b7f138c618075b0b1c88ec03e6b4
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-05-11 19:40:18 +0400

    handle refresh state change

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Navbar() {
   const { logout } = useLogout()
+  const {user} = useAuthContext()
 
   const handleClick = ()=>{
     logout()
@@ -13,13 +15,14 @@ export default function Navbar() {
         <div className="container">
             <Link to="/" ><h1>Workout Buddy</h1></Link>
             <nav>
-              <div>
+              {user &&(<div>
+                <span>{user.email}</span>
                 <button onClick={handleClick}>Logout</button>
-              </div>
-          <div>
+              </div>)}
+          {!user&&(<div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign up</Link>
-          </div>
+          </div>)}
         </nav>
         </div>
         
