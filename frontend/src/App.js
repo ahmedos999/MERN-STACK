c8c8bd6511e67268238f07c38726c8a400b44928
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-05-13 19:46:55 +0400

    redirect user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
+  const {user} = useAuthContext()
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,15 +15,15 @@ function App() {
         <Routes>
           <Route
           path='/'
-          element={<Home/>}
+          element={user?<Home/>:<Navigate to='/login'></Navigate>}
           ></Route>
           <Route
           path='/login'
-          element={<Login/>}
+          element={!user?<Login/>:<Navigate to='/'></Navigate>}
           ></Route>
           <Route
           path='/signup'
-          element={<Signup></Signup>}
+          element={!user?<Signup></Signup>:<Navigate to='/'></Navigate>}
           ></Route>
         </Routes>
       </div>
